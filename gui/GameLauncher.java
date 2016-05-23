3ed71d3820000a7a2ecf23b31761af2e91baf1fd
LB Nguyen <ninjatuna2014@gmail.com>
2016-05-23 12:40:22 -0500

    updated game launcher

--- gui/GameLauncher.java
+++ gui/GameLauncher.java
@@ -1,16 +1,24 @@
 import javax.swing.JOptionPane;
+import java.net.InetAddress;
 
 public class GameLauncher{
 	public static void main(String[] args){
+		String name = JOptionPane.showInputDialog(null, "Enter in your name");
 		int option = JOptionPane.showConfirmDialog(null, "Host the game?", "Monopoly!", JOptionPane.YES_NO_OPTION);
 		if(option == JOptionPane.YES_OPTION){
-		//	GameServer game = new GameServer(99);
-		//	Monopoly frame = new Monopoly("Monopoly!", "", 0);
+			GameServer server = new GameServer(99);
+			Thread t = new Thread(server);
+			t.start();
+			try{
+			System.out.println("GameServer started\n" + 
+				"Connected to: " + InetAddress.getLocalHost() + " and port 99");
+			}
+			catch(Exception e) { e.printStackTrace(); }
+			Monopoly frame = new Monopoly("Monopoly!", "localhost", 99, name);
 		}
 		else{
 			String IP = JOptionPane.showInputDialog(null, "Enter the IP Address of the host");
 			int port = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the port of the host"));
-			String name = (String)JOptionPane.showInputDialog(null,"Enter the name");
 			Monopoly frame = new Monopoly("Monopoly!", IP, port, name);
 		}
 	}
